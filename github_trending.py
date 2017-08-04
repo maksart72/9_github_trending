ffdb2b72b4bdfe55d42299ab4e8ddb083d73e20b
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-08-04 17:09:32 +0500

    fix

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,19 @@
 import requests
+import datetime
+import json
 
 def get_trending_repositories(top_size):
-    pass
+    delta = datetime.timedelta(days=7)
+    date = datetime.date.today()-delta
+    url = 'https://api.github.com/search/repositories'
+    details = {'q':'created:>'+str(date),'sort':'stars', 'order':'desc'}
+    r = requests.get(url,params=details)
+    data = json.loads(r.content)
+    print(data)
 
 def get_open_issues_amount(repo_owner, repo_name):
     pass
 
 if __name__ == '__main__':
-    r = requests.get('https://ya.ru/')
-    print(r.text)
+    top_size = 20
+    get_trending_repositories(top_size)
