7db5c53f2c88d60c5c8fd6ef56293392303633d7
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-08-04 14:52:04 +0500

    fix

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,11 @@
-
+import requests
 
 def get_trending_repositories(top_size):
     pass
-
 
 def get_open_issues_amount(repo_owner, repo_name):
     pass
 
 if __name__ == '__main__':
-    pass
+    r = requests.get('https://ya.ru/')
+    print(r.text)
